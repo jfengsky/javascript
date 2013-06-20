1d4a916067a1bbe34b87c7aa23d5f901423d0e2b
jfeng <jfengsky@gmail.com>
2013-06-20 19:09:08 +0800

    modify formcheck

--- formcheck/formcheck.js
+++ formcheck/formcheck.js
@@ -1,16 +1,31 @@
 $(function(){
+
   /**
    * [ 表单验证对象]
    * @return
    */
   var Formcheck = function(){
-    var _self = this,
-        checkMsg = {
-          err_name: '请填写商品名称，20字以内',
-          err_email: '请填写正确的Email',
-          err_mobile: '请填写正确的手机号码',
-          err_desp: '描述不能超过200字'
-        };
+    var _self = this;
+    this.errMsg = function(){
+      checkMsg = {
+        ERROR_NAME: {
+          0: "请填写商品名称，20字以内",
+          1: "商品名称不符合规范"
+        },
+        ERROR_EMAIL: {
+          0: "请填写正确的Email"
+        },
+        ERROR_MOBILE: {
+          0: "请填写正确的手机号码"
+        },
+        ERROR_DESP: {
+          0: "描述不能超过200字",
+          1: "描述不能包含特殊字符"
+        }
+      };
+      return checkMsg;
+    };
+        
     /**
      * [ 表单元素 ]
      * @param  {string} select  [表单的选择器]
@@ -28,15 +43,35 @@ $(function(){
       return checks;
     };
     this.init = function(){
-      var checkarr = [],  // 创建数组，写入所有表单对属性
+      var checkarr = [],  // 写入所有表单对属性
           inputs = $('#J_list :input');
+          
       // 遍历表单，写入属性
-      $(inputs).each(function(index){
+      $(inputs).each(function(){
         var must = ( /^\*/.test( $(this).prev().text() )) ? 1 : 0;
         checkarr.push( _self._elem($(this).attr('name'), must, 0) );
       });
-      console.log(checkarr);
+      return checkarr;
+      // console.log(checkarr);
     };
   };
-  new Formcheck().init();
+
+
+  $('#J_submit').click(function(){
+    var forms = new Formcheck(),
+        formsInit = forms.init(),
+        formsMsg = forms.errMsg();
+
+    $(formsInit).each(function(index){
+      if($(this)[0].correct === 0 && $(this)[0].must === 1){
+        if($(this)[0].id == 'nickname'){
+          $(':input[name=' + $(this)[0].id + ']').nextAll('.errtip').text(formsMsg['ERROR_NAME'][$(this)[0].errcode]);
+        };
+        if($(this)[0].id === 'email'){
+          $(':input[name=' + $(this)[0].id + ']').nextAll('.errtip').text(formsMsg['ERROR_EMAIL'][$(this)[0].errcode]);      
+        };
+      }
+    });
+  });
+
 });
