01e4695ca5302b678381dedc040e150e5c13593d
jfeng <jfengsky@gmail.com>
2013-08-11 14:01:26 +0800

    modify prototype.md

--- prototype.md
+++ prototype.md
@@ -1,3 +1,5 @@
-### 函数
+### 面向对象
+
+原文地址:[阮一峰的网络日志](http://www.ruanyifeng.com/blog/2010/05/object-oriented_javascript_encapsulation.html)
 
 
--- prototype/demo.js
+++ prototype/demo.js
@@ -1,54 +1,45 @@
+// function Animal(){
+//   this.species = '动物';
+// }
+
 // function Cat(name, color){
 //   this.name = name;
 //   this.color = color;
-//   this.specil = "猫科";
 // }
 
-// var catA = new Cat('大毛', '黄色');
-// var catB = new Cat('二毛', '黑色');
 
-// console.log(catA.specil);
-// console.log(catB.specil);
-// catA.specil = "犬科";
-// console.log(catA.specil);
-// console.log(catB.specil);
-// var cat1 = new Cat('大毛', '黄色');
-
-// // console.log(cat1.type);
-
-// Cat.prototype.type = "猫科动物";
-// Cat.prototype.eat = function(){
-//   console.log("老鼠");
-// };
-
-// var cat2 = new Cat('二毛', '黑色');
-// var cat3 = new Cat('二毛', '黑色');
-// // console.log(cat2.type);
-
-// // console.log(cat2.eat == cat3.eat);
-
-// // console.log( Cat.prototype.isPrototypeOf(cat1));
-
-// console.log(cat2.hasOwnProperty('name'));
-// console.log(cat2.hasOwnProperty('type'));
-
-
-
-
-
-// function Dog(name){
+// 1
+// function Cat(name, color){
+//   Animal.apply(this, argument);
 //   this.name = name;
+//   this.color = color;
 // }
-// Dog.prototype = {
-//   special: "犬科"
-// };
-// var dogA = new Dog('大毛');
-// var dogB = new Dog('二毛');
 
-// console.log(dogA.special);
-// console.log(dogB.special);
 
-// Dog.prototype.special = "猫科";
+// 2
 
-// console.log(dogA.special);
-// console.log(dogB.special);
+// Cat.prototype = new Animal();
+// Cat.prototype.constructor = Cat;
+// 
+// 
+// 3
+
+// function Animal(){};
+// Animal.prototype.species = "动物";
+
+// // Cat.prototype = Animal.prototype;
+// // Cat.prototype.constructor = Cat;
+
+// var F = function(){};
+// F.prototype = Animal.prototype;
+
+// Cat.prototype = new F();
+
+
+function extend(Child, Parent){
+  var F = function(){};
+  F.prototype = Parent.prototype;
+  Child.prototype = new F();
+  Child.prototype.constructor = Child;
+  Child.uber = Parent.prototype;
+}
