fa576c6e4b13b0f599e7a9e19ff97412676c7ddf
jfeng <jfengsky@gmail.com>
2013-10-13 10:33:05 +0800

    add README

--- indexedDB/README.md
+++ indexedDB/README.md
@@ -10,6 +10,47 @@
   		}
 	}, false)
 	
-####最终判断
+####最终判断和使用
 	
+	var idbSupported = false;
+	document.addEventListener('DOMContentLoaded', function(){
+  		if('indexedDB' in window){
+    		idbSupported = true;
+  		}
+	}, false);
 	
+打开一个indexedDB数据库是异步操作，目前有四种不同类型的事件监听事件
+
+* success
+* error
+* upgradeneeded  首次打开数据库或者改变数据库版本的时候被触发
+* blocked 在前一个连接没有被关闭的时候被触发
+
+各种事件的操作：
+
+	var idbSupported = false,
+    db;
+	document.addEventListener('DOMContentLoaded', function(){
+  		if('indexedDB' in window){
+    		idbSupported = true;
+  		}
+
+  		if(idbSupported){
+    		var openRequest = indexedDB.open('test', 1);
+
+    		openRequest.onupgradeneeded = function(e){
+      			console.log('upgrading...');
+    		}
+
+    		openRequest.onsuccess = function(e){
+      			console.log('success');
+      			db = e.target.result;
+    		}
+
+    		openRequest.onerror = function(e){
+      			console.log('error');
+      			console.dir(e);
+    		}
+  		}
+
+	}, false);
--- indexedDB/demo.js
+++ indexedDB/demo.js
@@ -1,9 +1,50 @@
 // 判断浏览器是否支持indexedDB
+// document.addEventListener('DOMContentLoaded', function(){
+//   if('indexedDB' in window){
+//     console.log('indexedDB success')
+//   } else {
+//     console.log('indexedDB false ')
+//   }
+// }, false)
+
+// --------------------------------------------------------
+
+// var idbSupported = false;
+// document.addEventListener('DOMContentLoaded', function(){
+//   if('indexedDB' in window){
+//     idbSupported = true;
+//   }
+// }, false);
+
+// --------------------------------------------------------
+
+var idbSupported = false,
+    db;
 document.addEventListener('DOMContentLoaded', function(){
   if('indexedDB' in window){
-    console.log('indexedDB success')
-  } else {
-    console.log('indexedDB false ')
+    idbSupported = true;
   }
-}, false)
 
+  if(idbSupported){
+    var openRequest = indexedDB.open('test_v2', 1);
+
+    openRequest.onupgradeneeded = function(e){
+      console.log('running onupgradeneeded');
+      var thisDB = e.target.result;
+      if(!thisDB.objectStoreNames.contains('firstOS')){
+        thisDB.createObjectStore('firstOS')
+      }
+    }
+
+    openRequest.onsuccess = function(e){
+      console.log('success');
+      db = e.target.result;
+    }
+
+    openRequest.onerror = function(e){
+      console.log('error');
+      console.dir(e);
+    }
+  }
+
+}, false);
