22814845ea01fa30c85688bd2eccc46749fc7aeb
jfeng <jfengsky@gmail.com>
2013-11-01 15:50:24 +0800

    add angular module

---
+++ angularjs/module/main.js
@@ -0,0 +1,9 @@
+define(function(require, exports, module) {
+  var angular = require('angular');
+  
+  // function Move(id, obj, fn){
+  //   $(id).animate(obj, 100, 'swing', fn);
+  // };
+  
+  module.exports = Move;
+});
