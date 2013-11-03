db9d8ea19bf755c3c12187cd26a4e27eb174ae80
jfeng <jfengsky@gmail.com>
2013-11-03 23:47:58 +0800

    ng-app modify

--- angularjs/module/script.js
+++ angularjs/module/script.js
@@ -1,10 +1,24 @@
-// declare a module
-var myAppModule = angular.module('myApp', []);
- 
-// configure the module.
-// in this example we will create a greeting filter
-myAppModule.filter('greet', function() {
- return function(name) {
-    return 'Hello, ' + name + '!';
-  };
-});
+define(function(require) {
+//  var angular = require('angular');
+  var myAppModule = angular.module('myApp',[]);
+  myAppModule.controller('TextController', function($scope){
+    var someText = {};
+    someText.message = "start angular";
+    $scope.someText = someText;
+  })
+  angular.bootstrap(document, ['myApp']);
+
+//var myAppModule = angular.module('myApp',[]);
+//myAppModule.controller('TextController', function($scope){
+//  var someText = {};
+//  someText.message = "start angular";
+//  $scope.someText = someText;
+})
+
+//});
+//var myAppModule = angular.module('myApp',[]);
+//myAppModule.controller('TextController', function($scope){
+//  var someText = {};
+//  someText.message = "start angular";
+//  $scope.someText = someText;
+//})
