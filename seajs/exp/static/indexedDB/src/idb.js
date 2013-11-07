0f9d2c6a7142afc754a35b9ea4d117330039bb33
jfeng <jfengsky@gmail.com>
2013-11-07 17:55:46 +0800

    db modify

--- seajs/exp/static/indexedDB/src/idb.js
+++ seajs/exp/static/indexedDB/src/idb.js
@@ -1,20 +1,60 @@
 define(function(require, exports, module){
-  console.log('idb loaded');
+
+  var $ = require('jquery');
   require('indexeddb');
-  module.exports = function(DBname, Ver, ){
-    return {
-      saveStore: function(){
 
-      },
-      getStoreVer: function(){
+  function Idb(){
+    var self = this;
+    this.loadFromCache = function(dbname, dbversion, key, data, fn){
+      // open data
+      var dbOpen = $.indexedDB(dbname, {
+        "version": dbversion,
+        // "upgrade" : function(transaction){
+        //   console.log('upgrade')
+        //   console.log(transaction);
+        // },
+        "schema": {
+          1: function(trans){
+            var objectStore = trans.createObjectStore(key, {
+              "keyPath": "name"
+              // "autoIncrement": true
+            });
+            // objectStore.createIndex("name");
+          }
+        }
+      }).done(function(db, event){
+        // get version
+        var lastDBVersion = db.version;
+        // check version
+        console.log(lastDBVersion);
+        console.log(dbversion);
+        if(lastDBVersion == dbversion){
+          $.indexedDB(dbname).objectStore(key).each(function(item){
+            console.log(item.value);
+            return item.value;
+          })
 
-      },
-      getStore: function(){
+          // TODO 直接从缓存获取新数据
+        } else {
+          // TODO 从api获取新的数据并写入缓存
+          console.log('false');
 
-      },
-      delStore: function(){
+          // 数据写入离线缓存
+          $.indexedDB(dbname).objectStore(key).put(data);
+        }
 
-      }
-    }
+        fn();
+        return data;
+
+      });
+
+      
+    };
+
+    this.deleteFromCache = function(dbname, key){
+      var deletePromise = $.indexedDB(dbname).deleteDatabase();
+    };
   };
+
+  module.exports = Idb;
 });
--- seajs/exp/static/indexedDB/src/main.js
+++ seajs/exp/static/indexedDB/src/main.js
@@ -1,10 +1,27 @@
 define(function(require) {
 
-  var $ = require('jquery');
-  var idb = require('./idb');
-  
+  var $ = require('jquery'),
+      idb = require('./idb');
+  var school = new idb();
+  console.log(school);
+
   $('#J_createDB').click(function(){
-    console.log(111);
+    var db_name = $('#J_dbname').val();
+    var key_name = $('#storename').val();
+    var datas = {
+      "name": "asdfa",
+      "age": 23
+    };
+    school.loadFromCache(db_name, 1, key_name, datas, function(){
+      console.log('loadfromecache');
+    });
   });
 
+
+  $('#J_delIndeedDB').click(function(){
+    var db_name = $('#J_dbname').val();
+    var key_name = $('#storename').val();
+    console.log('del db');
+    school.deleteFromCache(db_name, key_name);
+  });
 });
