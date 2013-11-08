328c8c0db3d12305247d778928342cb2a9d7d64e
jfeng <jfengsky@gmail.com>
2013-11-08 15:47:51 +0800

    indexeddb modify

---
+++ seajs/exp/app/getlanguage.php
@@ -0,0 +1,17 @@
+{
+  "timestamp": 123456,
+  "update_token": "echos_2321",
+  "resource": {
+    "student": "terry",
+    "version": "1.12" ,
+    "items":[
+      {
+        "age": 23,
+        "sex": "male",
+        "NO": 201204002,
+        "address": "shanghai 1023"
+      }
+      
+    ]
+  }
+}
--- seajs/exp/static/indexedDB/src/main.js
+++ seajs/exp/static/indexedDB/src/main.js
@@ -3,16 +3,28 @@ define(function(require) {
   var $ = require('jquery'),
       idb = require('./idb');
   var school = new idb();
-  console.log(school);
 
   $('#J_createDB').click(function(){
     var db_name = $('#J_dbname').val();
-    var key_name = $('#storename').val();
+    var store_name = $('#storename').val();
+    var key_name = $('#keyname').val();
+    var key_version = "1.12";
     var datas = {
-      "name": "asdfa",
-      "age": 23
-    };
-    school.loadFromCache(db_name, 1, key_name, datas, function(){
+          "db": "school",
+          "version": 1.01,
+          "items": [
+            {
+              "store": "student",
+              "value": [
+                {"ver":1.00, "name": "Tom", "age": 28},
+                {"ver":1.01, "name": "terry", "age": 18},
+                {"ver":1.02, "name": "joe", "age": 38}
+              ]
+            }
+          ]
+        };
+
+    school.loadFromCache(db_name, store_name, key_name, key_version, function(){
       console.log('loadfromecache');
     });
   });
