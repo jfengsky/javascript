3283021e8f1281a8a598fcdf7c57a41c623ab34e
jfeng <jfengsky@gmail.com>
2014-03-07 13:18:38 +0800

    add jstestdriver

---
+++ jstestdriver/demo.js
@@ -0,0 +1,12 @@
+/**
+ * Description:
+ * Author: jiangfeng(jiang.f@ctrip.com)
+ * Date: 2014-03-07 10:53
+ *
+ */
+
+describe('this is a demo',function(){
+  it('demo it',function(){
+    expect(false).toBeTruthy();
+  })
+});
