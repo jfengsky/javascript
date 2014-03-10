37f752e67fda426937ba78df7798e1c3c2f178e0
jfeng <jfengsky@gmail.com>
2014-03-10 15:56:45 +0800

    add handlebarstutor

---
+++ karma/karmatest/add.js
@@ -0,0 +1,10 @@
+/**
+ * Description:
+ * Author: jiangfeng(jiang.f@ctrip.com)
+ * Date: 2014-03-10 11:31
+ *
+ */
+
+function add(a,b){
+  return a+b;
+}
---
+++ karma/karmatest/test.js
@@ -0,0 +1,15 @@
+/**
+ * Description:
+ * Author: jiangfeng(jiang.f@ctrip.com)
+ * Date: 2014-03-10 11:31
+ *
+ */
+describe("A suite of basic functions", function() {
+  it("reverse word",function(){
+    expect(add(1,2)).toEqual(3);
+  });
+  it("to be true", function(){
+    expect(true).toBe(true);
+  })
+});
+
