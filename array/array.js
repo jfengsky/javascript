f3bbb60c47c2a0f2f0b7413aa84e92f33ea9681b
nicoleefang <nicoleefang@gmail.com>
2016-08-20 09:17:19 +0800

    array modify

--- array/array.js
+++ array/array.js
@@ -1,43 +1,13 @@
-var arr1 = ['c', 'a', 'b', 'd'];
-var arr2 = [['a', 'b'], ['c', 'd', 'e'], ['f', 'g']];
-var arr3 = [1, 2, 3];
+let arr1 = [1, 2, 3, 4, 5, 6, 7, 8]
 
-console.log(arr1);
-console.log(arr2);
+//arr1.forEach( (item, index, array)=>{
+//    console.log(item);
+//    console.log(index);
+//    console.log(array)
+//})
 
-// delete(arr1[1]);
-// console.log(arr1);
+let filterArray = arr1.filter( item => {
+    return  item < 4
+})
 
-// arr1.push('xx');
-// arr1.unshift('yy');
-// var arr4 = arr1.concat(arr2);
-// var arr4 = arr1.pop();
-// var arr4 = arr1.shift();
-// console.log(arr1);
-// arr1.splice(1,0,'xyz');
-// var arr4 = arr1.slice(0,2);
-// arr1.sort();
-// arr1.reverse();
-// console.log(arr1.toLocaleString());
-
-// console.log(arr1.indexOf('d'));
-// console.log(arr1.lastIndexOf('c'));
-
-// var arr4 = arr2.every(function(element, index, array){
-//   console.log(element);
-//   console.log(index);
-//   console.log(array);
-// });
-// console.log(arr4);
-
-// arr1.forEach(function(value, index, array){
-//   console.log(value);
-//   console.log(index);
-//   console.log(array);
-// });
-
-arr1.map(function(value, index, array){
-  console.log(value);
-  console.log(index);
-  console.log(array);
-});
+console.log(filterArray)
